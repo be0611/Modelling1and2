8b4900c1cc95b067228f1def335b1d9e4de0ce13
be0611 <bernardo.g.araujo@icloud.com>
2025-03-19 16:40:55 +0100

    Added better __init__

--- color_point.py
+++ color_point.py
@@ -1,25 +1,35 @@
 from point import Point
 import random
 
+class PointException(Exception):
+    pass
+
 class ColorPoint(Point):
     def __init__(self, x, y, color):
-        self.x = x
-        self.y = y
+        if not isinstance(x,(int,float)):
+            raise TypeError("x must be a number")
+        if not isinstance(y,(int,float)):
+            raise TypeError("y must be a number")
+
+        super().__init__(x,y)
         self.color = color
 
     def __str__(self):
         return f"<{self.color}: {self.x}, {self.y}>"
 
 p = ColorPoint(1,2,"red")
+
+
+
 print(p)
-colors = ["red", "green", "blue", "yellow", "black", "magenta",
-          "cyan", "white", "burgundy", "periwinkle", "marsala"]
-color_points = []
-for i in range(10):
-    color_points.append(
-        ColorPoint(random.randint(-10,10),
-                                   random.randint(-10,10),
-                                   random.choice(colors)))
-print(color_points)
-color_points.sort()
-print(color_points)
+# colors = ["red", "green", "blue", "yellow", "black", "magenta",
+#          "cyan", "white", "burgundy", "periwinkle", "marsala"]
+# color_points = []
+# for i in range(10):
+#    color_points.append(
+#        ColorPoint(random.randint(-10,10),
+#                                   random.randint(-10,10),
+#                                   random.choice(colors)))
+# print(color_points)
+# color_points.sort()
+# print(color_points)
